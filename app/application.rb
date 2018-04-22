aa4fe30c7e7bd20538f632415b660157fdbca3fd
Shun Yao <ysmiracle@hotmail.com>
2018-04-22 22:24:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     resp.write "#{num2}\n"
     resp.write "#{num3}\n"
 
-    if num_1==num_2 && num_2==num_3
+    if num1==num2 && num2==num3
       resp.write "You Win"
     else
       resp.write "You Lose"
